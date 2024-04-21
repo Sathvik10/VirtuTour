3fa437a64ff5b50ae59996361a4ac27f34bbaf9b
Romaanchan Skanda <romaanchanskanda@gmail.com>
2024-04-21 00:40:36 -0500

    Added other landmarks
    Added more landmarks for the routes

--- VirtuTour/app/constants/map/places.js
+++ VirtuTour/app/constants/map/places.js
@@ -9,6 +9,114 @@ const SterlingCEvansLibrary = {
     description: "Named after Sterling C. Evans, a devoted advocate for education, this library is more than a collection of books; it's a vibrant center for learning and discovery. Look around and you'll see students engaged in research, collaborating on projects, or delving into our vast digital and print collections that cater to a wide range of academic interests. Our library prides itself on providing state-of-the-art resources, including cutting-edge technology, multimedia stations, and a variety of study spaces to suit every need, from quiet reading rooms to dynamic group study areas. As we move through the library, you'll notice the harmonious blend of traditional architecture with modern facilities, reflecting our commitment to preserving history while embracing the future. Remember, the Sterling C. Evans Library is not just a place to study; it's a space where ideas are born, knowledge is shared, and the Aggie spirit of community and learning thrives."
 }
 
+const AcademicPlaza = {
+    name: "AcademicPlaza",
+    latitude: 30.615467693414473, 
+    longitude: -96.34135938960932, 
+    image: require('../images/AcadPlaza.jpg'),
+    track : Asset.fromModule(require('../../constants/audio/AcademicPlaza.mp3')),
+    description: "This is The centerpiece for main campus and gathering spot for students studying and relaxing. The plaza continues to serve as a venue for a number of traditions. The Academic Plaza is home to a statue of Lawrence Sullivan Ross, or Sully, as students call him. Sully was a former Texas Ranger, Governor of Texas and President of Texas A&M. During his presidency, way back in the 1890s, the Aggie Ring was designed, the Fightin' Texas Aggie Band was formed, and intercollegiate football was brought to campus. Silver Taps, one of Texas A&M's most honored traditions that pays tribute to a passing current student, also takes place here. A rendition of Taps, known as Silver Taps, is played from the dome of the Academic Building at the end of the ceremony. The plaza is surrounded by significant architectural features, including the large oak tree known as the Century Tree, which has become a symbol of lasting love and commitment among Aggie couples. The Academic Building, with its ionic columns and classical pediment, adds to the aesthetic and historical richness of the plaza. In recent years, there has been a focus on enhancing the Academic Plaza's role as a central gathering place. In 2021, Texas A&M's Board of Regents announced plans to invest about $1 million to reimagine the plaza, adding more green spaces and historical context to enrich its significance."
+}
+
+const AcademicBuild = {
+    name: "AcademicBuilding",
+    latitude: 30.615782490095075, 
+    longitude: -96.34080482904672,
+    image: require('../images/AcademicBuilding.jpg'),
+    track : Asset.fromModule(require('../../constants/audio/xyz.mp3')),
+    description: "The Academic Building at Texas A&M University is a significant structure with a rich architectural design that reflects the university's historical and cultural heritage. Designed by campus architect Frederick E. Giesecke and Samuel E. Gideon, the building was constructed in 1914 after the original Old Main building was destroyed by fire in 1912. The building showcases a Beaux-Arts classical design, which was a popular architectural style in the United States during the late 19th and early 20th centuries, characterized by grandiose and elaborate detailing. The Academic Building's history is intertwined with the evolution of Texas A&M University. It stands on the site of the former Old Main building, which was an integral part of the university before it was consumed by fire. The Academic Building has been a central part of Texas A&M's history for over a century and is one of only seven buildings on campus that are more than 100 years old. It has witnessed numerous significant events and traditions, including Silver Taps, a solemn ceremony honoring deceased students, which is held outside the building."
+}
+
+const StudentRecCentre = {
+    name: "StudentRecreationCentre",
+    latitude: 30.607318567654037, 
+    longitude: -96.34285004506536,
+    image: require('../images/StudentRec.jpg'),
+    track : Asset.fromModule(require('../../constants/audio/StudentRec.mp3')),
+    description: "The Student Recreation Center at Texas A&M University, affectionately known as The Rec, is a state-of-the-art facility that spans over 540,000 square feet, making it one of the nation's premier recreational sports facilities. It boasts a diverse array of amenities including a 32,000-square-foot strength and conditioning room, an indoor track, five swimming pools for various aquatic activities, and a 44-foot-high climbing wall with extensive climbing space. Additionally, The Rec features 10 multi-purpose indoor courts, 14 racquetball, handball, and squash courts, outdoor basketball courts, sand volleyball courts, and a dedicated boxing area. Membership is primarily available to the Texas A&M community, with students gaining automatic access through a Rec Sports Fee included in their tuition, while faculty, staff, alumni, and community members can purchase memberships. The center's history reflects its growth and adaptation to the evolving needs of the university's population, with numerous expansions and renovations undertaken to maintain its high standard of recreational offerings. The Rec serves as a vibrant center for wellness and community engagement, underlining its importance to the campus life at Texas A&M University."
+}
+
+const CushingLibrary = {
+    name: "CushingLibrary",
+    latitude: 30.616526672403207, 
+    longitude: -96.33925801556485,
+    image: require('../images/CushingLibrary.jpg'),
+    track : Asset.fromModule(require('../../constants/audio/CushingLibrary.mp3')),
+    description: "The Cushing Memorial Library and Archives at Texas A&M University, named in honor of Edward B. Cushing, a pivotal figure in the university's history, is a central repository for rare books, special collections, manuscripts, and archival materials. Originally constructed in 1930 and rededicated in 1998, the library has expanded significantly, now housing one of the world's largest collections of science fiction and fantasy, among other notable collections in military history, Texana, and various cultural studies. Located at 400 Spence St., College Station, TX, the library offers a range of services including reading room access, tours, and educational events, supporting the university's broad academic community. With over 170,000 printed volumes, 21,000 feet of manuscript material, and extensive photographic and art collections, Cushing Library serves as a vital resource for research across numerous disciplines. It also maintains a strong digital presence, providing online access to its catalog and information about its services and collections."
+}
+
+const SullivanRoss = {
+    name: "SullivanRossStatue",
+    latitude: 30.61564624889684, 
+    longitude: -96.34060193425167,
+    image: require('../images/SulRoss.jpg'),
+    track : Asset.fromModule(require('../../constants/audio/SulRoss.mp3')),
+    description: "Lawrence Sullivan Ross, commonly known as Sul Ross, was a significant figure in Texas history, serving as a Confederate general during the Civil War, the 19th governor of Texas, and later as the president of the Agricultural and Mechanical College of Texas, now known as Texas A&M University. Born on September 27, 1838, in Iowa Territory, Ross moved to Texas as a child and later became known for his roles in both military and educational leadership. At Texas A&M University, a statue of Ross, dedicated in 1918, stands prominently in the Academic Plaza and is one of the oldest sculptures on campus. However, the statue has also been a point of contention due to Ross's Confederate past and the broader implications for racial justice and historical memory. In recent years, particularly following nationwide protests for racial justice, there have been calls from students and community members to remove the statue, citing Ross's Confederate ties and actions during his military career that are viewed as racially oppressive."
+}
+
+const ReedArena = {
+    name: "ReedArena",
+    latitude: 30.60622637769857, 
+    longitude: -96.34594818924687,
+    image: require('../images/ReedArena.jpg'),
+    track : Asset.fromModule(require('../../constants/audio/ReedArena.mp3')),
+    description: "Reed Arena is a prominent sports arena and entertainment venue located on the Texas A&M University campus at 730 Olsen Boulevard, College Station, Texas. the arena is situated at the corner of Olsen Boulevard and Kimbrough Boulevard. Opened on November 22, 1998, Reed Arena was designed by the architectural firm Lockwood, Andrews & Newman, Inc., and constructed at a cost of $36.7 million, which is equivalent to $65.9 million in 2022 dollars. Owned and operated by Texas A&M University, Reed Arena has a seating capacity of 12,989 and features a hardwood playing surface. It serves as the home ground for the Texas A&M Aggies men's and women's basketball teams, as well as the Aggies volleyball team. The arena is not only used for sporting events but also hosts a variety of other activities, including concerts, family entertainment, trade shows, student programs, and commencement ceremonies. Reed Arena has also been recognized for its energetic atmosphere during basketball games, partly due to the enthusiastic student group known as the Reed Rowdies"
+}
+
+const BonfireMemorial = {
+    name: "BonfireMemorial",
+    latitude: 30.62297526609954, 
+    longitude: -96.33453045114656,
+    image: require('../images/Bonfire.jpg'),
+    track : Asset.fromModule(require('../../constants/audio/Bonfire.mp3')),
+    description: "The Bonfire Memorial at Texas A&M University is a poignant tribute to the 12 students who tragically lost their lives during the collapse of the Aggie Bonfire on November 18, 1999. This memorial not only commemorates those who died but also serves as a symbol of the Aggie Spirit and the deep sense of community at Texas A&M. The design of the Bonfire Memorial is thoughtful and symbolic, incorporating several elements that reflect the tradition, history, and spirit of the Aggie Bonfire. Visitors enter the memorial through the Tradition Plaza, which marks the entrance and reflects on the activities that bring the Aggie community together. This area features a granite Spirit Wall engraved with lines from The Spirit of Aggieland, and panels engraved with The Last Corps Trip, a poem traditionally recited before the lighting of the Bonfire. The Bonfire Memorial is not just a physical structure but a profound representation of the Aggie community's resilience and unity. It stands as a reminder of the tragic event that deeply affected Texas A&M and led to the reevaluation of the Bonfire tradition. The memorial also serves as a place for reflection and remembrance, offering a space where the Aggie family can come together to honor those who lost their lives."
+}
+
+const CorpOfCadets = {
+    name: "CorpOfCadets",
+    latitude: 30.614679608612985, 
+    longitude: -96.33952322210168,
+    image: require('../images/CorpOfCadets.jpg'),
+    track : Asset.fromModule(require('../../constants/audio/CorpofCadets.mp3')),
+    description: "The Corps of Cadets at Texas A&M University, known as the Keepers of the Spirit and Guardians of Tradition, is a prestigious and integral part of the university's heritage and student life. Established in 1876, the same year as the university itself, the Corps has a long-standing tradition of developing well-educated leaders of character who are prepared for the global leadership challenges of the future. The Corps arches serve as the entry point to the Corps of Cadets Quadrangle, or Quad. This is home to the approximately 2,200 men and women who participate on a voluntary basis in the Corps of Cadets. The Corps of Cadets is the nation's largest uniformed student organization and consistently commissions more officers into the U.S. Armed Forces than any other institution, except for the nation's service academies. New residence halls, as well as the Buzbee Leadership Learning Center, have recently been added to the Quad to accommodate the growing number of incoming students who want to be members of the Corps."
+}
+
+const CenturySquare = {
+    name: "CenturySquare",
+    latitude: 30.62576579576516, 
+    longitude: -96.33827699630308,
+    image: require('../images/CenturySquare.jpg'),
+    track : Asset.fromModule(require('../../constants/audio/CenturySquare.mp3')),
+    description: "Now, we are at the Century Square. Century Square is an exciting mixed-use destination adjacent to Texas A&M University in College Station, Texas. Redefining the Brazos Valley, Century Square creates a dynamic community center where people congregate from across the region to experience a walkable, urban destination. Century Square is a public-private collaboration between Midway and The Texas A&M University System, utilizing previously undeveloped land to deliver substantial benefits and an enriched quality of life for residents and the entire Brazos Valley region. The development of Century Square is a public-private collaboration between Midway and The Texas A&M University System. This partnership has utilized previously undeveloped land to deliver substantial benefits to the community. The project is managed by Parkway, whose leadership includes former students of Texas A&M, deeply engaged in the university's culture and the growth of the Brazos Valley"
+}
+
+const CommonsDining = {
+    name: "Commons",
+    latitude: 30.615290454008107, 
+    longitude: -96.33568105230674,
+    image: require('../images/Commons.jpg'),
+    track : Asset.fromModule(require('../../constants/audio/Commons.mp3')),
+    description: "The Commons houses four, suite-style residence halls, dining and Living Learning Communities. These halls are a popular choice on campus due to nightly tutors and plenty of places to study and relax. It also has dining options. So if you are living in these dorm rooms and you have aggie dining dollars then you can get your food from here. Similarly there are other residencies around the campus that you can stay at depending on your preferences. You also have multiple dining places around where you can redeem your dining dollars for food."
+}
+
+const AggieSpirit = {
+    name: "AggieSpirit",
+    latitude: 30.614070661496793, 
+    longitude: -96.34149277643637,
+    image: require('../images/aggieSpirit.jpg'),
+    track : Asset.fromModule(require('../../constants/audio/AggieSpirit.mp3')),
+    description: "The Aggie Spirit bus system at Texas A&M University provides comprehensive transportation services for students, faculty, staff, and visitors on and around the campus. The Aggie Spirit transit system operates both on-campus and off-campus routes. The on-campus service is free for all and consists of many routes that transport passengers around the university. In addition to the regular bus routes, the Aggie Spirit system also offers paratransit services for students, faculty, and staff with temporary or permanent disabilities, as well as charter services for university and student organization events. "
+}
+
+const RingStatue = {
+    name: "RingStatue",
+    latitude: 30.608825088368494, 
+    longitude: -96.33592437589844,
+    image: require('../images/RingStatue.jpg'),
+    track : Asset.fromModule(require('../../constants/audio/RingStatue.mp3')),
+    description: "The Ring Statue is located in the Haynes Ring Plaza behind the Williams Alumni Center. The Aggie Ring is a highly significant symbol for Texas A&M University and its students and alumni. The Aggie Ring was first introduced in 1889 and has become a cherished tradition at Texas A&M. It is seen as a symbol of camaraderie, dedication, and the Aggie Spirit. The ring features several images engraved on it, each with special meaning - the torch represents knowledge, the eagle represents the United States, and the school's founding date is also included. Receiving the Aggie Ring is an important milestone for Texas A&M students. Wearing the ring is a source of great pride for Aggies, as it signifies their membership in the Aggie family and their commitment to the university's values. A 12-foot-tall bronze replica of the Aggie Ring provides a picturesque backdrop for Aggie Ring Day. It is inscribed with Harold J. “Bill” Haynes, distinguished alumnus and namesake for the plaza. The seal at the top of the ring bears his class year, 1946, and a time capsule inside to be opened 2046. Quite an interesting place isn't it"
+}
+
 const BrightBuilding = {
     name: "Bright Building",
     latitude: 30.619065771971126, 
@@ -121,7 +229,19 @@ const locations = [SterlingCEvansLibrary,
     ZachryBuilding,
     HaynesEngineeringBuilding,
     FoxRunBasketBallCourt,
-    London2311
+    London2311,
+    RingStatue,
+    AggieSpirit,
+    CommonsDining,
+    CenturySquare,
+    CorpOfCadets,
+    BonfireMemorial,
+    ReedArena,
+    SullivanRoss,
+    CushingLibrary,
+    StudentRecCentre,
+    AcademicBuild,
+    AcademicPlaza
     ]
 
 export {
@@ -137,5 +257,17 @@ export {
     ZachryBuilding,
     HaynesEngineeringBuilding,
     FoxRunBasketBallCourt,
-    London2311
+    London2311,
+    RingStatue,
+    AggieSpirit,
+    CommonsDining,
+    CenturySquare,
+    CorpOfCadets,
+    BonfireMemorial,
+    ReedArena,
+    SullivanRoss,
+    CushingLibrary,
+    StudentRecCentre,
+    AcademicBuild,
+    AcademicPlaza
 }
